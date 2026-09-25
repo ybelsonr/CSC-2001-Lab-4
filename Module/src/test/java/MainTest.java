4b467182b150a03b44d56527794592e148f8d8a5
ybelsonr <ybelsonr@calpoly.edu>
2026-09-24 22:06:28 -0700

    Initial commit

---
+++ Module/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void addSix() {
+        assertEquals(45, Main.addSix(39));
+    }
+
+}
